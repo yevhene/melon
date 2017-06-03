b17787989fbdc16ce8a37c0538f4fc25d141dc48
Yevhen Shemet <yevhene@gmail.com>
2017-06-03 13:21:42 +0300

    Domain app

---
+++ apps/domain/lib/domain.ex
@@ -0,0 +1,18 @@
+defmodule Bonus.Domain do
+  @moduledoc """
+  Documentation for Bonus.Domain.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Bonus.Domain.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/domain/test/domain_test.exs
@@ -0,0 +1,8 @@
+defmodule Bonus.DomainTest do
+  use ExUnit.Case
+  doctest Bonus.Domain
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
