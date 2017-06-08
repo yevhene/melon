fb6bf0278ad699cc62fa9f49c05d5bb1b37fe525
Yevhen Shemet <yevhene@gmail.com>
2017-06-09 01:13:18 +0300

    Customers Cards

---
+++ apps/domain/lib/domain/customers/card.ex
@@ -0,0 +1,13 @@
+defmodule Melon.Domain.Customers.Card do
+  use Ecto.Schema
+
+  schema "accounts_cards" do
+    field :number, :string
+    field :is_active, :boolean
+
+    belongs_to :identity, Melon.Domain.Customers.Identity
+    belongs_to :issuer, Melon.Domain.Partners.Point
+
+    timestamps(type: :utc_datetime)
+  end
+end
---
+++ apps/domain/priv/repo/migrations/20170608220959_create_customers_cards.exs
@@ -0,0 +1,18 @@
+defmodule Melon.Domain.Repo.Migrations.CreateCustomersCards do
+  use Ecto.Migration
+
+  def change do
+    create table(:customers_cards) do
+      add :number, :text, null: false
+      add :is_active, :boolean, null: false, default: true
+
+      add :identity_id, references(:customers_identites), null: false
+      add :issuer_id, references(:partners_points)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:customers_cards, [:number, :identity_id], unique: true)
+    create index(:customers_cards, [:identity_id])
+  end
+end
