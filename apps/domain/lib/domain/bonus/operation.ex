2b9f77b521d9a75a938e1cc000bf5919b9e791cb
Yevhen Shemet <yevhene@gmail.com>
2017-06-09 01:45:32 +0300

    Bonus Operations

---
+++ apps/domain/lib/domain/bonus/operation.ex
@@ -0,0 +1,13 @@
+defmodule Melon.Domain.Bonus.Operation do
+  use Ecto.Schema
+
+  schema "bonus_operations" do
+    field :value, :integer
+    field :posted_at, :utc_datetime
+
+    belongs_to :account, Melon.Domain.Customers.Account
+    belongs_to :point, Melon.Domain.Partners.Point
+
+    timestamps(type: :utc_datetime)
+  end
+end
---
+++ apps/domain/priv/repo/migrations/20170608223053_create_bonus_operations.exs
@@ -0,0 +1,19 @@
+defmodule Melon.Domain.Repo.Migrations.CreateBonusOperations do
+  use Ecto.Migration
+
+  def change do
+    create table(:bonus_operations) do
+      add :value, :integer, null: false
+      add :posted_at, :utc_datetime, null: false,
+        default: fragment("CURRENT_TIMESTAMP")
+
+      add :account_id, references(:customers_accounts), null: false
+      add :point_id, references(:partners_points), null: false
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:bonus_operations, [:account_id])
+    create index(:bonus_operations, [:point_id])
+  end
+end
